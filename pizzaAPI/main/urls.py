63c7b5dcff15c88c28a6ea7ee17ddf98709b99e5
Maciej <maxter1@interia.pl>
2022-01-15 03:34:34 +0100

    update_pizza_type fixed

--- pizzaAPI/main/urls.py
+++ pizzaAPI/main/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("", views.homepage, name="homepage"),
     path("menu/", views.menu, name="menu"),
     path("add-pizza-type/", views.add_pizza_type, name="add_pizza_type"),
+    path("update-pizza-type/<str:pk>/", views.update_pizza_type, name="update_pizza_type"),
+    path("delete-pizza-type/<str:pk>/", views.delete_pizza_type, name="delete_pizza_type"),
 ]
--- pizzaAPI/main/views.py
+++ pizzaAPI/main/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.http import JsonResponse
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -27,7 +25,34 @@ def menu(request):
 
 @api_view(['POST'])
 def add_pizza_type(request):
-    serializer = PizzaTypeSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
+    if request.user.is_superuser:
+        serializer = PizzaTypeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response("You don't have authority to add new pizza to menu.")
+
+
+@api_view(['POST'])
+def update_pizza_type(request, pk):
+    if request.user.is_superuser:
+        pizza_type = PizzaType.objects.get(id=pk)
+        serializer = PizzaTypeSerializer(instance=pizza_type, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response("Pizza type updated.")
+        else:
+            return Response("Update failed. Invalid data.")
+    else:
+        return Response("You don't have authority to update pizza in menu.")
+
+
+@api_view(['DELETE'])
+def delete_pizza_type(request, pk):
+    if request.user.is_superuser:
+        pizza_type = PizzaType.objects.get(id=pk)
+        pizza_type.delete()
+        return Response("Pizza type deleted.")
+    else:
+        return Response("You don't have authority to delete this tasty pizza.")
